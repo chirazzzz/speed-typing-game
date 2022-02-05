9bc00f65a814986849a27911ce0887e922f260fb
Chirag Mehta <chiragmehta.uk@gmail.com>
2022-02-05 14:07:42 +0000

    added typedText state, handleChange & small styling changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [typedText, setTypedText] = useState("");
+
+  function handleChange(event) {
+    const {value} = event.target
+    setTypedText(value);
+  }
+
   return (
-    <div className="App">
+    <div>
       <h1>Speed Typing Game</h1>
-      <textarea />
-      <h4>Time remaining:</h4>
-      <button>Start Game</button>
-      <h2>Word Count:</h2>
+      <textarea onChange={handleChange} value={typedText} />
+      <h4>Time reminaing: ???</h4>
+      <button>Start</button>
+      <h1>Word count: ???</h1>
     </div>
   );
 }
