c7204b4e058827b607ffb1e77bb8d0815b9661aa
Chirag Mehta <chiragmehta.uk@gmail.com>
2022-02-05 14:47:24 +0000

    created wordCount state & func which deals with bugs

--- src/App.js
+++ src/App.js
@@ -3,10 +3,23 @@ import "./App.css";
 
 function App() {
   const [typedText, setTypedText] = useState("");
+  const [wordCount, setWordCount] = useState(0);
 
   function handleChange(event) {
-    const {value} = event.target
+    const { value } = event.target;
     setTypedText(value);
+  }
+
+  function countWords(str) {
+    // regex to rreduce multiple spaces to a single space.
+    str = str.replace(/[ ]{2,}/gi, " ");
+    // trim() will take away any spaces at start or end of strinf
+    str = str.trim();
+    // split returns array with space between which we then .length
+    setWordCount(str.split(" ").length);
+    if (str === "") {
+      setWordCount(0);
+    }
   }
 
   return (
@@ -14,8 +27,8 @@ function App() {
       <h1>Speed Typing Game</h1>
       <textarea onChange={handleChange} value={typedText} />
       <h4>Time reminaing: ???</h4>
-      <button>Start</button>
-      <h1>Word count: ???</h1>
+      <button onClick={() => countWords(typedText)}>Start</button>
+      <h1>Word count: {wordCount}</h1>
     </div>
   );
 }
